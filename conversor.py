cd353be466716f6ecdadd20e572259774571d836
Cristian Trejo <cristrejo.tec@gmail.com>
2020-07-13 20:11:15 -0500

    Programa para convertir dolares a pesos y viceversa

--- conversor.py
+++ conversor.py
@@ -1,8 +1,13 @@
-pesos = input('¿Cuántos pesos colombianos tienes? ')
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos /valor_dolar
+def conversionMonedas(opcion, cantidad):
+    if opcion == 1:
+        conversion = round(cantidad / 22.73, 2)
+        print(f'{cantidad} MXN equivalen a {conversion} USD.')
+    elif opcion == 2:
+        conversion = round(cantidad * 22.73, 2)        
+        print(f'{cantidad} USD equivalen a {conversion} MXN.')
+    else:
+        print(f'Opción o cantidad no valida.')
 
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print('Tienes $' + dolares + ' dólares')
+opcionUsuario = int(input('Elije una opción: \n1- Convertir de MXN a USD. \n2- Convertir de USD a MXN.\n'))
+cantidadUsuario = float(input('Ingresa la cantidad a convertir: '))
+conversionMonedas(opcionUsuario, cantidadUsuario)
